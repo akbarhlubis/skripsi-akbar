f27ab41b7ff6bf101a9ca64740303c9e841524c0
akbarhlubis <akbar_hamonangan_lubis@teknokrat.ac.id>
2023-11-04 22:56:56 +0700

    fix footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,9 +4,9 @@
     <aside>
       <img class="w-20" src="https://teknokrat.ac.id/wp-content/uploads/2022/04/UNIVERSITASTEKNOKRAT-e1647677057867-768x774-min.png" alt="">
       <h2 class="mb-2 text-2xl">{{Config::get('app.name')}}</h2>
-      <p class="mb-1  md:w-[40%] w-full">{{$setting->description}}</p>
+      <p class="mb-1  md:w-[40%] w-full">{{App\Models\Setting::findOrFail(1)->description}}</p>
       <div class="grid grid-flow-col gap-4">
-        <a href="https://www.instagram.com/{{$setting->instagram}}/"><i class="bi bi-instagram"></i></a> 
+        <a href="https://www.instagram.com/{{App\Models\Setting::findOrFail(1)->instagram}}/"><i class="bi bi-instagram"></i></a> 
         <a href="https://www.youtube.com/channel/UCYVRIU8V_Td9fHJRA2u57cg"><i class="bi bi-youtube"></i></a> 
         <a href="https://www.facebook.com/Teknokrat"><i class="bi bi-facebook"></i></a>
       </div>
@@ -23,8 +23,8 @@
         <header class="footer-title">Hubungi Kami</header> 
         <div class="grid grid-flow-row gap-4">
           <p>Direktorat Kemahasiswaan</p>
-          <a href="https://wa.me/{{$setting->phone}}">{{$setting->phone}} (Chat Only)</a>
-          <a href="mailto:{{$setting->email}}">{{$setting->email}}</a>
+          <a href="https://wa.me/{{App\Models\Setting::findOrFail(1)->phone}}">{{App\Models\Setting::findOrFail(1)->phone}} (Chat Only)</a>
+          <a href="mailto:{{App\Models\Setting::findOrFail(1)->email}}">{{App\Models\Setting::findOrFail(1)->email}}</a>
         </div>
       </div>
     </nav>
