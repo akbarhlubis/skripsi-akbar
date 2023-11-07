107ca20b27494cc7d2c870a9150ee1f950026e59
akbarhlubis <akbar_hamonangan_lubis@teknokrat.ac.id>
2023-11-07 07:40:59 +0700

    disable barcode

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
 @section('container')
 {{-- Halaman about tentang komunitas SIMANEV UTI --}}
 <div class="container pt-20 mx-auto">
-    <div class="min-h-screen w-[50%] mx-auto text-justify">
+    <div class="min-h-screen px-10 w-full md:w-[50%] mx-auto text-justify">
         <h1 class="text-3xl font-semibold text-center text-primary">Tentang Kami</h1>
         <p class="pt-2">Sistem Manajemen Event atau Event Management merupakan sistem berbasis website yang berguna untuk <b>mengelola</b> dan <b>mempublikasikan kegiatan</b> yang diselenggarakan oleh <b>Kemahasiswaan Universitas Teknokrat Indonesia</b>.</p>
         <p class="pt-2">Projek ini dikerjakan oleh <b>Akbar Hamonangan Lubis</b> menggunakan <b>Framework Laravel</b> dan <b>TailwindCSS</b> serta metode Extreme Programmings</p>
--- resources/views/components/table-users.blade.php
+++ resources/views/components/table-users.blade.php
@@ -10,10 +10,10 @@
                 <span class="text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase">Alamat
                     Email</span>
             </th>
-            <th class="px-6 py-3 text-left bg-gray-50">
+            {{-- <th class="px-6 py-3 text-left bg-gray-50">
                 <span class="text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase">
                     Kode Barcode</span>
-            </th>
+            </th> --}}
             <th class="px-6 py-3 text-left bg-gray-50">
                 <span class="text-xs font-medium leading-4 tracking-wider text-gray-500 uppercase">Tombol
                     Aksi</span>
@@ -33,13 +33,13 @@
                         {{ $regis->user->email }}
                     </span>
                 </td>
-                <td class="px-6 py-3 text-left">
+                {{-- <td class="px-6 py-3 text-left">
                     <span class="text-xs font-medium leading-4 tracking-wider">
                         <img class="w-20"
                             src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={{ $regis->barcode }}"
                             alt="" srcset="">
                     </span>
-                </td>
+                </td> --}}
                 <td class="px-6 py-3 text-left">
                     <a href="{{route('event.is-attend',['event' => $event->id, 'user_id' => $regis->user->id])}}" class="text-xs font-medium leading-4 tracking-wider">
                         {{ $regis->is_attended ? 'Menghadiri' : 'Tandai Hadir' }}
